84a3bf6d0d91997d3cacb87c1e820b813fbb8d68
RuthraMoorthyK <moorthy@securden.com>
2025-02-10 16:44:35 +0530

    done

--- Formula/my-cli.rb
+++ Formula/my-cli.rb
@@ -2,11 +2,11 @@ class MyCli < Formula
   desc "Your CLI tool"
   homepage "https://github.com/John12356/homebrew-my-cli"
   url "https://github.com/John12356/homebrew-my-cli/releases/download/v1.0.0/my-cli-1.0.0-darwin-amd64.tar.gz"
-  sha256 "C26AFA30BD770D34C82DA509F50396D0F5FB96EA44C83999126EB5CA8C78437C"
+  sha256 "C90BBF01C5167E2D6A3DB767A2465B35EFE79DEEEEF7EC616DE4C255241165F7"
   license "MIT"
   version "1.0.0"
 
   def install
-    bin.install "bin/my-cli"
+    bin.install "my-cli"
   end
 end
