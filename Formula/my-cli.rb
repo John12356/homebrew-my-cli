aacc6e75d82cc762755fc06ba2cc27f5b5e8c45d
RuthraMoorthyK <moorthy@securden.com>
2025-02-11 10:55:57 +0530

    new version changes and issues fix

--- Formula/my-cli.rb
+++ Formula/my-cli.rb
@@ -2,7 +2,7 @@ class MyCli < Formula
   desc "Your CLI tool"
   homepage "https://github.com/John12356/homebrew-my-cli"
   url "https://github.com/John12356/homebrew-my-cli/releases/download/v1.0.0/my-cli-1.0.0-darwin-amd64.tar.gz"
-  sha256 "C90BBF01C5167E2D6A3DB767A2465B35EFE79DEEEEF7EC616DE4C255241165F7"
+  sha256 "FCC340E06BD6ABB92870A13314A88FB3E9B416EC9C88EED5C45486183F0F514A"
   license "MIT"
   version "1.0.0"
 
